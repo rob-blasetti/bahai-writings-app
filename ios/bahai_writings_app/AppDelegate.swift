b71f5091304ca95705f14b5765fbc6d36f19b694
Rob Blasetti <rblasett@gmail.com>
2025-11-05 14:14:46 +1100

    rename app to Kali and add temp logo

--- ios/bahai_writings_app/AppDelegate.swift
+++ ios/bahai_writings_app/AppDelegate.swift
@@ -24,7 +24,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     window = UIWindow(frame: UIScreen.main.bounds)
 
     factory.startReactNative(
-      withModuleName: "bahai_writings_app",
+      withModuleName: "Kali",
       in: window,
       launchOptions: launchOptions
     )
