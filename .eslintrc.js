d29bb81b9ebdb91eace7af34aefd1a4560029efb
Rob Blasetti <rblasett@gmail.com>
2026-02-03 11:45:23 +1100

    chore: declare globalThis for eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,8 @@
 module.exports = {
   root: true,
   extends: '@react-native',
+  globals: {
+    // RN/JSC supports globalThis, but some eslint configs don't declare it.
+    globalThis: 'readonly',
+  },
 };
